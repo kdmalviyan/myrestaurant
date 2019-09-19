a10a7b5793d73ae78709f48d26958ebb3860c6c5
Manoj Prabhakar Singh <manoj.singh@tavant.com>
2019-09-19 18:06:37 +0530

    Commiting Order project

---
+++ my-restaurant-order/src/main/java/com/myrestaurant/myrestaurantorder/MyRestaurantOrderApplication.java
@@ -0,0 +1,13 @@
+package com.myrestaurant.myrestaurantorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestaurantOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestaurantOrderApplication.class, args);
+	}
+
+}
