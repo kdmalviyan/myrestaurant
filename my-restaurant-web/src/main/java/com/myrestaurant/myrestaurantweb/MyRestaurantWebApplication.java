bb21c0df2381276ae60237ef98d59107528c3d47
Kuldeep Singh <>
2019-09-19 17:43:42 +0530

    Initial commit

---
+++ my-restaurant-web/src/main/java/com/myrestaurant/myrestaurantweb/MyRestaurantWebApplication.java
@@ -0,0 +1,13 @@
+package com.myrestaurant.myrestaurantweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestaurantWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestaurantWebApplication.class, args);
+	}
+
+}
