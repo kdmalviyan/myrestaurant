3aaa24c4cbde8db6398ca084ad8d7157e31c7811
Vipin Kumar <vipin.kumar@tavant.com>
2019-09-19 18:11:36 +0530

    commiting payment modul.

---
+++ my-restaurant-payment/src/main/java/com/myrestaurant/myrestaurantpayment/MyRestaurantPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.myrestaurant.myrestaurantpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestaurantPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestaurantPaymentApplication.class, args);
+	}
+
+}
