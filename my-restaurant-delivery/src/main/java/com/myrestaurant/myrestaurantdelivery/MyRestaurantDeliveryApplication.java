cccde33ba097b61ab6f49ce5a75d3b8e88459058
Kuldeep Singh <>
2019-09-25 14:42:07 +0530

    Eureka Client enabled

--- my-restaurant-delivery/src/main/java/com/myrestaurant/myrestaurantdelivery/MyRestaurantDeliveryApplication.java
+++ my-restaurant-delivery/src/main/java/com/myrestaurant/myrestaurantdelivery/MyRestaurantDeliveryApplication.java
@@ -2,8 +2,10 @@ package com.myrestaurant.myrestaurantdelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MyRestaurantDeliveryApplication {
 
 	public static void main(String[] args) {
--- my-restaurant-user-management/src/main/java/com/myrestaurant/myrestaurantusermanagement/MyRestaurantUserManagementApplication.java
+++ my-restaurant-user-management/src/main/java/com/myrestaurant/myrestaurantusermanagement/MyRestaurantUserManagementApplication.java
@@ -2,10 +2,12 @@ package com.myrestaurant.myrestaurantusermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.myrestaurant.*")
+@EnableEurekaClient
 public class MyRestaurantUserManagementApplication {
 
 	public static void main(String[] args) {
