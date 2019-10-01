f6b56ff0fc84c2c4d0e3cd1d2dc42f52652ed07e
manoj <manoj.singh8890@gmail.com>
2019-10-01 10:20:07 +0530

    comiting

--- restaurant-api-gateway/src/main/java/com/myrestauran/apigateway/RestaurantApiGatewayApplication.java
+++ restaurant-api-gateway/src/main/java/com/myrestauran/apigateway/RestaurantApiGatewayApplication.java
@@ -2,14 +2,16 @@ package com.myrestauran.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class RestaurantApiGatewayApplication {
 
+public class RestaurantApiGatewayApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApiGatewayApplication.class, args);
 	}
